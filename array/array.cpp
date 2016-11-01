8cf405c94e195f6f0db066717726f864150abac4
ethan-cui <dawa880306@gmail.com>
2016-10-31 20:19:49 -0400

    some more questions

--- array/array.cpp
+++ array/array.cpp
@@ -1,4 +1,5 @@
 #include <vector>
+#include <string>
 #include <algorithm>
 #include "../utils.hpp"
 
@@ -51,6 +52,31 @@ public:
 			res.push_back(x);
 		return res;
 	}
+
+	vector<vector<int>> combine_1(int n, int k){
+		vector<vector<int>> res;
+		if (n<=0 || k<0 || k>n)
+			return res;
+		vector<int> valueList;
+		combineHelper_1(n, k, 1, &res, valueList);
+
+		return res;
+
+	}
+
+	void combineHelper_1(int n, int k, int pos, vector<vector<int>> *res, vector<int> valueList){
+		if (k == 0){
+			res->push_back(valueList);
+			return;
+		}
+
+		for (int i=pos; i<=n; i++){
+			vector<int> valueList_temp(valueList);
+			valueList_temp.push_back(i);
+			combineHelper_1(n, k-1, i+1, res, valueList_temp);
+		}
+	}
+
 
 
 	// 39. combinationSum
@@ -119,21 +145,244 @@ public:
 		}
 	}
 
+	// 216. combinationSum3
+	//vector<vector<int>> combinationSum3(int k, int n){
+		//vector<vector<int>> res;
+
+		//if (k <=0 || n<=0 || n > 45)
+			//return res;
+		//vector<int> l = {1,2,3,4,5,6,7,8,9};
+
+		//vector<vector<int>> temp = combineHelper(k, l);
+		//for (auto x: temp){
+			//int sum = accumulate(x.begin(), x.end(), 2);
+			//if (sum == n)
+				//res.push_back(x);
+		//}
+		//return res;
+	//}
+
+	// 228. summaryRanges
+	vector<string> summaryRanges(vector<int>& nums){
+
+		vector<string> res;
+		string arrow("->");
+		if (nums.size() == 1){
+			res.push_back(to_string(nums[0]));
+			return res;
+		}
+		else{
+			bool restart=true;
+			int start=0;
+			int end=0;
+			for (unsigned i=0; i<nums.size(); i++){
+				if (restart) {
+					start = nums[i];
+					restart = false;
+				}
+				if (i<nums.size()-1){
+					if (nums[i+1]==nums[i]+1)
+						continue;
+					else{
+						if (start==nums[i]){
+							res.push_back(to_string(start));
+							restart=true;
+						}
+						else{
+							end=nums[i];
+							restart=true;
+							res.push_back(to_string(start) + arrow + to_string(end));
+						}
+					}
+				}
+				else{
+					if (start==nums[i])
+						res.push_back(to_string(start));
+					else{
+						end = nums[i];
+						res.push_back(to_string(start) + arrow + to_string(end));
+					}
+				}
+			}
+		}
+
+		return res;
+	}
+
+	vector<string> summaryRanges_1(vector<int>& nums){
+		vector<string> res;
+		if( nums.size() == 0) return res;
+
+		int i=0, j=0; // two pointers for positions
+		int max_pos = nums.size() - 1;
+		string arrow("->");
+
+		if (max_pos == 0) {
+			res.push_back(to_string(nums[i]));
+			return res;
+		}
+
+		while(i<=max_pos){
+			while ( j<max_pos && nums[j+1]-nums[j] == 1)
+				j++;
+			if (j == i)
+				res.push_back(to_string(nums[i]));
+			else
+				res.push_back(to_string(nums[i]) + arrow + to_string(nums[j]));
+			j++;
+			i=j;
+		}
+		return res;
+	}
+
+	vector<vector<int>> subsetsWithDup(vector<int>& nums){
+
+		vector<vector<int>> res;
+		if (nums.size() == 0) return res;
+		sort(nums.begin(), nums.end());
+
+		vector<int> subset;
+		subsetsWithDupHelper(nums, 0, &res, subset);
+		return res;
+	}
+
+	void subsetsWithDupHelper(vector<int> & nums, unsigned pos, vector<vector<int>> * res, vector<int> subset){
+		res->push_back(subset);
+		for (unsigned i=pos; i<nums.size(); i++){
+			if (i>pos && nums[i-1] == nums[i]) continue;
+			//subset.push_back(nums[i]);
+			//subsetsWithDupHelper(nums, i+1, res, subset);
+			//subset.pop_back();
+			
+			vector<int> subset_temp(subset);
+			subset_temp.push_back(nums[i]);
+			subsetsWithDupHelper(nums, i+1, res, subset_temp);
+		}
+	}
+
+	vector<vector<int>> permute(vector<int>& nums){
+		vector<vector<int>> res;
+		if (nums.size() == 0) return res;
+		vector<int> valueList;
+		permuteHelper(nums, 0, &res, valueList);
+		
+		return res;
+	}
+
+	void permuteHelper(vector<int>& nums, unsigned pos, vector<vector<int>> *res, vector<int> valueList){
+		if (pos == nums.size()){
+			res->push_back(valueList);
+			return;
+		}
+
+		for (unsigned i=pos; i<nums.size(); i++){
+			int temp = nums[pos] ^ nums[i];
+			nums[pos] ^= temp;
+			nums[i] ^= temp;
+			valueList.push_back(nums[pos]);
+			permuteHelper(nums, pos+1, res, valueList);
+			valueList.pop_back();
+			nums[pos] ^= temp;
+			nums[i] ^= temp;
+		}
+	}
+	vector<vector<int>> permute_1(vector<int>& nums){
+		vector<vector<int>> res;
+		if (nums.size() == 0) return res;
+		permuteHelper(nums, 0, &res);
+		
+		return res;
+	}
+
+	void permuteHelper(vector<int>& nums, unsigned pos, vector<vector<int>> *res){
+		if (pos == nums.size()){
+			res->push_back(nums);
+			return;
+		}
+		// we actually do not need the valueList, in nums, it's inplace swap.
+		for (unsigned i=pos; i<nums.size(); i++){
+			int temp = nums[pos] ^ nums[i];
+			nums[pos] ^= temp;
+			nums[i] ^= temp;
+			permuteHelper(nums, pos+1, res);
+			nums[pos] ^= temp;
+			nums[i] ^= temp;
+		}
+	}
+
+	vector<vector<int>> permuteUnique(vector<int>& nums){
+		vector<vector<int>> res;
+		if (nums.size() == 0) return res;
+
+		vector<int> valueList;
+		sort(nums.begin(), nums.end());
+		permuteUniqueHelper(nums, 0, &res, valueList);
+
+		return res;
+
+
+	}
+
+	void permuteUniqueHelper(vector<int>& nums,unsigned pos, vector<vector<int>> *res, vector<int> valueList){
+		if (pos == nums.size()){
+			res->push_back(valueList);
+			return;
+		}
+
+		for (unsigned i=pos; i<nums.size(); i++){
+			if (i>pos && nums[pos] == nums[i]) continue;
+			if (i>pos && nums[i-1] == nums[i]) continue;
+		
+
+			int temp = nums[pos] ^ nums[i];
+			nums[pos] ^= temp;
+			nums[i] ^= temp;
+			valueList.push_back(nums[pos]);
+			permuteUniqueHelper(nums, pos+1, res, valueList);
+			valueList.pop_back();
+			nums[pos] ^= temp;
+			nums[i] ^= temp;
+		}
+	}
 };
 
 int main(){
 
 	Solution s;
+	// 47 permuteUnique
+	vector<int> a = {1,2,2,1,3,3,4};
+	vector<vector<int>> temp = s.permuteUnique(a);
+
+	//// 46 permute
+	//vector<int> a = {1,2,3,4};
+	//vector<vector<int>> temp = s.permute_1(a);
+
+	////90. subsetsWithDup
+	//vector<int> a = {1,2,2,3,3};
+	//vector<vector<int>> temp = s.subsetsWithDup(a);
 
 	//// combine
 	//vector<vector<int>> temp = s.combine(2, 4);
+	//vector<vector<int>> temp = s.combine_1(20, 16);
 
 	// 39 combinationSum
 	//vector<int> a = {3,2, 6, 7};
 	//vector<vector<int>> temp = s.combinationSum(a, 7);
-	vector<int> a = {10, 1, 2, 7, 6, 1, 5};
-	vector<vector<int>> temp = s.combinationSum2(a, 8);
+
+	//// 40 combinationSum2
+	//vector<int> a = {10, 1, 2, 7, 6, 1, 5};
+	//vector<vector<int>> temp = s.combinationSum2(a, 8);
+	
+	//// 228. summaryRanges
+	//vector<int> v{1,2,3,4,7,8, 10};
+	//cout << s.summaryRanges(v) << endl;
+	//cout << s.summaryRanges_1(v) << endl;
+
+
+	//vector<vector<int>> temp = s.combinationSum3(3, 9);
 	//cout << temp.size() << endl;
+	
+
 	for(auto& x:temp)
 		cout << x << endl;
 
--- bitManipulation/bitManipulation.cpp
+++ bitManipulation/bitManipulation.cpp
@@ -1,4 +1,6 @@
 #include <vector>
+#include <unordered_set>
+#include <unordered_map>
 #include <cmath>
 #include <string>
 #include <iostream>
@@ -214,53 +216,6 @@ public:
 	}
 
 
-	vector<string> summaryRanges(vector<int>& nums){
-
-		vector<string> res;
-		string arrow("->");
-		if (nums.size() == 1){
-			res.push_back(to_string(nums[0]));
-			return res;
-		}
-		else{
-			bool restart=true;
-			int start=0;
-			int end=0;
-			for (unsigned i=0; i<nums.size(); i++){
-				if (restart) {
-					start = nums[i];
-					restart = false;
-				}
-				if (i<nums.size()-1){
-					if (nums[i+1]==nums[i]+1)
-						continue;
-					else{
-						if (start==nums[i]){
-							res.push_back(to_string(start));
-							restart=true;
-						}
-						else{
-							end=nums[i];
-							restart=true;
-							res.push_back(to_string(start) + arrow + to_string(end));
-						}
-					}
-				}
-				else{
-					if (start==nums[i])
-						res.push_back(to_string(start));
-					else{
-						end = nums[i];
-						res.push_back(to_string(start) + arrow + to_string(end));
-					}
-				}
-			}
-		}
-
-		return res;
-	}
-
-
 	bool isPowerOfTwo(int n){
 
 		if (n <= 0) return false;
@@ -429,10 +384,146 @@ public:
 
 	vector<int> singleNumberIII(vector<int>& nums){
 		// first compute a^b
+		vector<int> res;
 		int x = 0;
-		for (int elem:nums){
+		for (int elem:nums)
+			x^=elem; // This a^b
+		int i=x&~(x-1); // get the least signicant bit, use it to distinguish a and b
+		vector<int> p, q;
+		for (int elem: nums){
+			if(i&elem) p.push_back(elem);
+			else q.push_back(elem);
+		}
+
+		int a=0, b=0;
+		for (int elem:p) a^=elem;
+		for (int elem:q) b^=elem;
+
+		res.push_back(a);
+		res.push_back(b);
+
+		return res;
+	}
+
+	int findMaximumXOR(vector<int>& nums){
+
+		if (nums.size() == 0) return 0;
+		int max = 0;
+		int mask = 0;
+		unordered_set<int> prefix;
 
 		
+		for (int i=31; i>=0; i--){
+			// create mask
+			mask |= 1<<i;
+			prefix.clear();
+
+			for (int x:nums)
+				prefix.insert(x&mask);
+
+			int candidate = max | 1<<i;
+			
+			for (int x:prefix){
+				unordered_set<int>::const_iterator got = prefix.find(candidate^x);
+				if (got != prefix.end()){
+					max = candidate;
+					break;
+				}
+			}
+		}
+
+		return max;
+	}
+	// this one returns the a_i and a_j as well
+	vector<int> findMaximumXOR_test(vector<int>& nums){
+
+		int max = 0;int mask = 0;int a = 0;
+		vector<int> res;
+		unordered_set<int> prefix;
+
+		for (int i=31; i>=0; i--){
+			// create mask
+			mask |= 1<<i;
+			prefix.clear();
+
+			for (int x:nums)
+				prefix.insert(x&mask);
+
+			int candidate = max | 1<<i;
+			
+			for (int x:prefix){
+				unordered_set<int>::const_iterator got = prefix.find(candidate^x);
+				if (got != prefix.end()){
+					max = candidate;
+					a = x;
+					break;
+				} else {
+					unordered_set<int>::const_iterator got_1 = prefix.find(max^x);
+					if (got_1 != prefix.end())
+						a |= x & 1<<i;
+				}
+			}
+		}
+		res.push_back(max); res.push_back(a); res.push_back(a^max);
+		return res;
+	}
+
+	// 405 toHex
+	string toHex(int num){
+
+		if (num == 0) {
+			string value("0");
+			return value;
+		}
+		
+		int mask = 15;
+		unordered_map<int, string> dict;
+		dict[0] = string("0");
+		dict[1] = string("1");
+		dict[2] = string("2");
+		dict[3] = string("3");
+		dict[4] = string("4");
+		dict[5] = string("5");
+		dict[6] = string("6");
+		dict[7] = string("7");
+		dict[8] = string("8");
+		dict[9] = string("9");
+		dict[10] = string("a");
+		dict[11] = string("b");
+		dict[12] = string("c");
+		dict[13] = string("d");
+		dict[14] = string("e");
+		dict[15] = string("f");
+
+		vector<string> res;
+
+		for (int i=0; i<8; i++){
+			if (num == 0) break;
+			string v = dict[num&mask];
+			res.push_back(v);
+			num >>= 4;
+		}
+
+		string ret;
+		for (string& x:res)
+			ret = x + ret;
+		return ret;
+	}
+
+	string toHex_1(int num){
+		if (num == 0) {
+			string v("0");
+			return v;
+		}
+
+		char * label = "0123456789abcdef";
+		string res;
+		for (int i=0; i<8; i++){
+			if (num == 0) break;
+			res = string(1, label[num&15]) + res;
+			num >>= 4;
+		}
+		return res;
 	}
 
 
@@ -441,6 +532,28 @@ public:
 int main(){
 
 	Solution s;
+
+	//// for toHex
+	//cout << s.toHex(10) << endl;
+	//cout << s.toHex(0) << endl;
+	//cout << s.toHex(-1) << endl;
+	//cout << s.toHex(26) << endl;
+	//cout << s.toHex(1000) << endl << endl;
+
+	//cout << s.toHex_1(10) << endl;
+	//cout << s.toHex_1(0) << endl;
+	//cout << s.toHex_1(-1) << endl;
+	//cout << s.toHex_1(26) << endl;
+	//cout << s.toHex(1000) << endl;
+	
+	//// for singleNumberIII
+	//vector<int> nums = {1, 2, 1, 3, 2, 5};
+	//cout << s.singleNumberIII(nums) << endl;
+	
+	//// for findMaximumXOR
+	//vector<int> temp = {10, 32, 2234, 18, 28};
+	//cout << s.findMaximumXOR(temp) << endl;	
+	//cout <<  s.findMaximumXOR_test(temp) << endl;
 
 	//// for getSum
 	//cout << s.getSum(5,6) << 5+6 << endl;
@@ -453,10 +566,10 @@ int main(){
 	//cout << s.isPowerOfFour1(1<<30) << s.isPowerOfFour(1<<30) << endl;
 	//cout << s.isPowerOfFour1(1<<31) << s.isPowerOfFour(1<<31) << endl;
 
-	// for rangeBitwiseAnd
-	cout << s.rangeBitwiseAnd1(5,7) << s.rangeBitwiseAnd(5, 7) << endl;
-	cout << s.rangeBitwiseAnd1(9, 10) << s.rangeBitwiseAnd(9, 10) << endl;
-	cout << s.rangeBitwiseAnd1(9,9) << s.rangeBitwiseAnd(9, 9) << endl;
+	//// for rangeBitwiseAnd
+	//cout << s.rangeBitwiseAnd1(5,7) << s.rangeBitwiseAnd(5, 7) << endl;
+	//cout << s.rangeBitwiseAnd1(9, 10) << s.rangeBitwiseAnd(9, 10) << endl;
+	//cout << s.rangeBitwiseAnd1(9,9) << s.rangeBitwiseAnd(9, 9) << endl;
 	
 	//// for majorityElement
 	//vector<int> nums = {1,2,3,4,3,2,2,2,2,3,3,2,2,2,3,2,3,3};
@@ -464,12 +577,12 @@ int main(){
 
 
 
-	//// for subsets
-	//vector<int> nums=range(10);
-	//s.subsets(nums);
-	//s.subsets1(nums);
-	//s.subsets2(nums);
-	//
+	// for subsets
+	vector<int> nums=range(2);
+	s.subsets(nums);
+	s.subsets1(nums);
+	s.subsets2(nums);
+	
 	
 	//// for singleNumberII
 	//vector<int> nums = {1,1,1,3, 4,4,4,5,5,5};
@@ -504,11 +617,6 @@ int main(){
 	////cout << s.addDigits(81) << endl;
 	
 
-	//// summaryRanges
-	//vector<int> v{1,2,3,4,7,8};
-	//cout << v.size() << endl;
-
-	//cout << s.summaryRanges(v) << endl;
 
 	//// reverseBits
 	//for (unsigned i=0; i<1000000; i++)
@@ -519,17 +627,17 @@ int main(){
 
 
 
-	// isPowerOfTwo
-	cout << s.isPowerOfTwo(343232435) << endl;
-	cout << s.isPowerOfTwo1(343232435) << endl;
-	cout << s.isPowerOfTwo(-16) << endl;
-	cout << s.isPowerOfTwo1(-16) << endl;
-	cout << s.isPowerOfTwo(1024) << endl;
-	cout << s.isPowerOfTwo1(1024) << endl;
-	cout << s.isPowerOfTwo(23) << endl;
-	cout << s.isPowerOfTwo1(23) << endl;
-	cout << s.isPowerOfTwo(8) << endl;
-	cout << s.isPowerOfTwo1(8) << endl;
+	//// isPowerOfTwo
+	//cout << s.isPowerOfTwo(343232435) << endl;
+	//cout << s.isPowerOfTwo1(343232435) << endl;
+	//cout << s.isPowerOfTwo(-16) << endl;
+	//cout << s.isPowerOfTwo1(-16) << endl;
+	//cout << s.isPowerOfTwo(1024) << endl;
+	//cout << s.isPowerOfTwo1(1024) << endl;
+	//cout << s.isPowerOfTwo(23) << endl;
+	//cout << s.isPowerOfTwo1(23) << endl;
+	//cout << s.isPowerOfTwo(8) << endl;
+	//cout << s.isPowerOfTwo1(8) << endl;
 	
 	//// hammingWeight
 	//cout << s.hammingWeight(343232435) << endl;
